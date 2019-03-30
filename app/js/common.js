9d25e46605b0494d54587d9373a3eb08ad8fabef
Elesmer <paelesmer@gmail.com>
2019-03-30 12:18:50 +0200

    3.1
    Add responsive navbar, add burger for mobile devices, add to top button.

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,17 @@
-$(function() {
+$(document).ready(function () {
 
-	// Custom JS
+  // Resize sticky header
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 250) {
+      $('header').addClass("sticky");
+    } else {
+      $('header').removeClass("sticky");
+    }
 
+    if ($(this).scrollTop() > 500) {
+      $('.totop').addClass("uk-animation-slide-bottom");
+    } else {
+      $('.totop').removeClass("uk-animation-slide-bottom");
+    }
+  });
 });
